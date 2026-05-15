36c3d0a5c9f45585eb809b896b466b572328c77d
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2026-05-15 00:04:46 -0500

    .env.example updated

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,12 @@ ALLOWED_HOSTS = config(
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:8000,http://127.0.0.1:8000',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
+
 
 # ---------------------------------------------------------------------------
 # Application definition
